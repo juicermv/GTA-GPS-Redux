8ec4dc6938af56dc85489ec5ba86c4a88f15fab1
juicermv <yonimeller@protonmail.com>
2025-01-21 23:10:15 +0200

    fixes

--- project.lua
+++ project.lua
@@ -1,6 +1,5 @@
 local PLUGIN_SDK_DIR = network:zip("https://github.com/juicermv/plugin-sdk/releases/latest/download/msvc.zip")
-local SIMDStringX86 = network:zip("https://github.com/juicermv/SIMDString/releases/download/release/release.zip").."\\SIMDStringX86"
-
+local SIMDStringX86 = network:zip("https://github.com/juicermv/SIMDString/releases/download/release/msvc.zip")
 
 local project = Project(
     "GPS Redux",
@@ -16,6 +15,7 @@ local project = Project(
         PLUGIN_SDK_DIR .. "/src/",
         PLUGIN_SDK_DIR .. "/src/plugin_sa/",
         PLUGIN_SDK_DIR .. "/src/plugin_sa/game_sa/",
+        PLUGIN_SDK_DIR .. "/src/plugin_sa/game_sa/rw/",
         PLUGIN_SDK_DIR .. "/src/shared/",
         PLUGIN_SDK_DIR .. "/src/shared/game/",
         "external/mini/src",
@@ -72,6 +72,49 @@ local project = Project(
     "x86",
     "DynamicLibrary"
 )
+
+tasks:create("build_mingw", function()
+    local PLUGIN_SDK_DIR_MINGW = network:zip("https://github.com/juicermv/plugin-sdk/releases/latest/download/mingw.zip")
+    local SIMDStringX86_MINGW = network:zip("https://github.com/juicermv/SIMDString/releases/download/release/mingw.zip")
+    project.include_paths = {
+        PLUGIN_SDK_DIR_MINGW .. "/src/",
+        PLUGIN_SDK_DIR_MINGW .. "/src/plugin_sa/",
+        PLUGIN_SDK_DIR_MINGW .. "/src/plugin_sa/game_sa/",
+        PLUGIN_SDK_DIR_MINGW .. "/src/plugin_sa/game_sa/rw/",
+        PLUGIN_SDK_DIR_MINGW .. "/src/shared/",
+        PLUGIN_SDK_DIR_MINGW .. "/src/shared/game/",
+        "external/mini/src",
+        SIMDStringX86_MINGW,
+        "src"
+    }
+    project.lib_paths = {
+        PLUGIN_SDK_DIR_MINGW .. "/lib/plugin_sa/",
+        "external/d3dx9",
+        SIMDStringX86_MINGW
+    }
+    project.arch = i686
+    project.libs = {
+        "d3d9",
+        "d3dx9",
+        "plugin",
+        "SIMDString"
+    }
+
+    project.compiler_flags = {
+        "-std=gnu++23",
+        "-Ofast",
+        "-fpermissive",
+        "-shared",
+        "-g",
+    }
+
+    project.linker_flags = {
+        "--dll",
+        "--subsystem,windows",
+        "--export-all-symbols"
+    }
+    mingw:build(project)
+end)
 
 tasks:create("build_msvc", function() 
     msvc:build(project)
